5d336333dd954b5641f205e1623ec46cea04e5d9
Mathias <siloimwald@gmail.com>
2026-06-19 20:16:41 +0200

    made lua lsp happier

--- scenes/book_one_final.lua
+++ scenes/book_one_final.lua
@@ -3,55 +3,54 @@ dofile('./scenes/scene_helpers.lua')
 
 Scene.samples_per_pixel = 10
 
-set_cam({ 13, 2, 3 }, { 0, 0, 0 }, { 0, 1, 0 }, 20, 10, 0.6)
+Set_Cam({ 13, 2, 3 }, { 0, 0, 0 }, { 0, 1, 0 }, 20, 10, 0.6)
 
-off_limits = { x = 4, y = 0.2, z = 0 }
+local off_limits = { x = 4, y = 0.2, z = 0 }
 
 -- cannot deal (yet) with one-off/throw away materials, need unique
 -- names, count up each time we use something for the random spheres
-mat_counter = 0
+local mat_counter = 0
 -- random spheres
 for a = -11, 10, 1 do
     for b = -11, 10, 1 do
-        material = math.random()
+        local material = math.random()
 
-        sphere_center = {
+        local sphere_center = {
             x = a + 0.9 * math.random(),
             y = 0.2,
             z = b + 0.9 * math.random()
         }
 
-        if distance(sphere_center, off_limits) > 0.9 then
-            m = {}
-            mat_name = string.format("material_%d", mat_counter)
+        if Vec_Distance(sphere_center, off_limits) > 0.9 then
+            local mat_name = string.format("material_%d", mat_counter)
             mat_counter = mat_counter + 1
             if material < 0.8 then
-                matte_color = color_mix(random_3(), random_3())
-                add_matte(mat_name, matte_color.x, matte_color.y, matte_color.z)
+                local matte_color = Color_Mix(Random_3f(), Random_3f())
+                Add_Matte(mat_name, matte_color.x, matte_color.y, matte_color.z)
             elseif material < 0.95 then
-                metal_color = random_3(0.5, 1)
-                add_metal(mat_name, metal_color.x, metal_color.y, metal_color.z, math.random() * 0.5)
+                local metal_color = Random_3f(0.5, 1)
+                Add_Metal(mat_name, metal_color.x, metal_color.y, metal_color.z, math.random() * 0.5)
             else
-                add_dielectric(mat_name, 1.5)
+                Add_Dielectric(mat_name, 1.5)
             end
 
-            add_sphere(sphere_center.x, sphere_center.y, sphere_center.z, 0.2, mat_name)
+            Add_Sphere(sphere_center.x, sphere_center.y, sphere_center.z, 0.2, mat_name)
         end
     end
 end
 
-ground = add_matte("ground", 0.5, 0.5, 0.5)
+local ground = Add_Matte("ground", 0.5, 0.5, 0.5)
 
-add_sphere(0, -1000, -1, 1000, ground)
+Add_Sphere(0, -1000, -1, 1000, ground)
 
 -- the three non-random spheres
-glass = add_dielectric("glass", 1.5)
-add_sphere(0, 1, 0, 1, glass)
+local glass = Add_Dielectric("glass", 1.5)
+Add_Sphere(0, 1, 0, 1, glass)
 
-matte = add_matte("matte", 0.4, 0.2, 0.1)
-add_sphere(-4, 1, 0, 1, matte)
+local matte = Add_Matte("matte", 0.4, 0.2, 0.1)
+Add_Sphere(-4, 1, 0, 1, matte)
 
-metal = add_metal("metal", 0.7, 0.6, 0.5, 0)
-add_sphere(4, 1, 0, 1, metal)
+local metal = Add_Metal("metal", 0.7, 0.6, 0.5, 0)
+Add_Sphere(4, 1, 0, 1, metal)
 
 return Scene
--- scenes/scene_helpers.lua
+++ scenes/scene_helpers.lua
@@ -16,7 +16,7 @@ Scene = {
     materials = {}
 }
 
-function set_cam(look_from, look_at, up, fov, focus_distance, defocus_angle)
+function Set_Cam(look_from, look_at, up, fov, focus_distance, defocus_angle)
     Scene.camera = {
         look_at = look_at,
         look_from = look_from,
@@ -27,17 +27,17 @@ function set_cam(look_from, look_at, up, fov, focus_distance, defocus_angle)
     }
 end
 
-function add_matte(name, r, g, b)
+function Add_Matte(name, r, g, b)
     Scene.materials[name] = { albedo = { r, g, b }, type = "Matte" }
     return name
 end
 
-function add_dielectric(name, ior)
+function Add_Dielectric(name, ior)
     Scene.materials[name] = { ior = ior, type = "Dielectric" }
     return name
 end
 
-function add_metal(name, r, g, b, fuzz)
+function Add_Metal(name, r, g, b, fuzz)
     Scene.materials[name] = {
         albedo = { r, g, b },
         fuzz = fuzz,
@@ -46,7 +46,7 @@ function add_metal(name, r, g, b, fuzz)
     return name
 end
 
-function add_sphere(x, y, z, radius, material)
+function Add_Sphere(x, y, z, radius, material)
     table.insert(Scene.spheres, {
         center = { x, y, z },
         radius = radius,
@@ -56,23 +56,23 @@ end
 
 -- math and color helpers
 
-function random_3(min, max)
+function Random_3f(min, max)
     min = min or 0
     max = max or 1
-    irange = max - min
-    x = min + math.random() * irange
-    y = min + math.random() * irange
-    z = min + math.random() * irange
+    local irange = max - min
+    local x = min + math.random() * irange
+    local y = min + math.random() * irange
+    local z = min + math.random() * irange
     return { x = x, y = y, z = z }
 end
 
 -- distance between point v and w
-function distance(v, w)
-    v_minus_w = { v.x - w.x, v.y - w.y, v.z - w.z }
-    lq = v_minus_w[1] * v_minus_w[1] + v_minus_w[2] * v_minus_w[2] + v_minus_w[3] * v_minus_w[3]
+function Vec_Distance(v, w)
+    local v_minus_w = { v.x - w.x, v.y - w.y, v.z - w.z }
+    local lq = v_minus_w[1] * v_minus_w[1] + v_minus_w[2] * v_minus_w[2] + v_minus_w[3] * v_minus_w[3]
     return math.sqrt(lq)
 end
 
-function color_mix(a, b)
+function Color_Mix(a, b)
     return { x = a.x * b.x, y = a.y * b.y, z = a.z * b.z }
 end
